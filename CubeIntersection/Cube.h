36cf8553f25d437e605b1e519b9a3c6596a6591f
Ionut Ivan <ionut.ivan@icloud.com>
2014-09-09 11:25:14 +0200

    Add Cube class

---
+++ CubeIntersection/Cube.h
@@ -0,0 +1,13 @@
+//
+//  Cube.h
+//  CubeIntersection
+//
+//  Created by Ionut Ivan on 09/09/14.
+//  Copyright (c) 2014 Ionut Ivan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cube : NSObject
+
+@end
---
+++ CubeIntersection/Cube.m
@@ -0,0 +1,13 @@
+//
+//  Cube.m
+//  CubeIntersection
+//
+//  Created by Ionut Ivan on 09/09/14.
+//  Copyright (c) 2014 Ionut Ivan. All rights reserved.
+//
+
+#import "Cube.h"
+
+@implementation Cube
+
+@end
