854a19e0ad11436ee03fe571e2545f1d78784d81
Ionut Ivan <ii@goldengekko.com>
2014-09-09 10:53:48 +0200

    first commit

---
+++ CubeIntersection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CubeIntersection
+//
+//  Created by Ionut Ivan on 09/09/14.
+//  Copyright (c) 2014 Ionut Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CubeIntersection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CubeIntersection
+//
+//  Created by Ionut Ivan on 09/09/14.
+//  Copyright (c) 2014 Ionut Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
